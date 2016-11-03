b39e3ac65b0ea029d5820a5fd9d66088535f60b2
belabonga <shania1faza@gmail.com>
2016-11-03 10:55:58 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,6 +3,9 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class HasilActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " lahir pada tahun " + tahunLahir);
+
+        setTitle("Hasil");
     }
 
     @Override
--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "Nama";
+    public static final String UMUR = "Umur";
+    EditText etNama;
+    EditText etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,27 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+                goHasil();
             }
         });
 
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
+
+        setTitle("Input");
+
+    }
+
+    private void goHasil() {
+        String nama = etNama.getText().toString();
+        String umurStr = etUmur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAMA, nama);
+        intent.putExtra(UMUR, umur);
+
+        startActivity(intent);
     }
 }

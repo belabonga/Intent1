86a4c29df7bebdb0d9e837da0d0279278413614d
belabonga <shania1faza@gmail.com>
2016-11-03 10:12:28 +0700

    onBackPressed Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.learn.intent1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class HasilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+
+        findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
+
 }
